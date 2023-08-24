a8ccad66b269661b694f34a2e47bc7a16a67d843
akporto <kellennporto@gmail.com>
2023-08-24 15:42:15 -0300

    [Alteração] Substituição do Bootstrap pelo Splide.js no carrossel de slides
    Neste commit, removemos a biblioteca Bootstrap que estava sendo usada para o carrossel de slides e a substituímos pela biblioteca Splide.js. Essa mudança foi realizada para melhorar o desempenho e oferecer mais flexibilidade na criação do carrossel de slides.
    
    Principais mudanças:
    - Remoção dos arquivos CSS e JS do Bootstrap relacionados ao carrossel
    - Inclusão dos arquivos CSS e JS do Splide.js
    - Configuração das opções do Splide.js para atender aos requisitos do projeto, incluindo o autoplay e a exibição de 3 slides em desktops e 1 slide em dispositivos móveis
    
    Essa mudança nos permite ter um carrossel responsivo e interativo sem a necessidade de carregar todo o framework do Bootstrap.

--- transfer.js
+++ transfer.js
@@ -6,3 +6,19 @@ document.addEventListener("DOMContentLoaded", function () {
     navbarNav.classList.toggle("show");
   });
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  new Splide('#image-carousel', {
+      type: 'loop', 
+      perPage: 3,   
+      perMove: 1,   
+      autoplay: true, 
+      interval: 3000, 
+      breakpoints: {
+          768: {
+              perPage: 1, 
+          }
+      }
+  }).mount();
+});
